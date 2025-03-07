ccf30a5a13bc626c34b5c281b58834318ca7d0cb
Alvi7171 <alvirahman7172@gmail.com>
2025-03-07 14:24:30 +0600

    Add files via upload
    smartcomhub_main

---
+++ Smart_Comm_Hub/script.js
@@ -0,0 +1,18 @@
+function navigateToAnnouncements() {
+    window.location.href = "announcements.html";
+}
+
+function goBack() {
+    window.location.href = "communication-hub.html";
+}
+
+
+function navigateToFAQ() {
+    window.location.href = "faq.html";
+}
+
+// Toggle FAQ Answers
+function toggleAnswer(id) {
+    let answer = document.getElementById(id);
+    answer.style.display = answer.style.display === "block" ? "none" : "block";
+}
